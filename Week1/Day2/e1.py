0339b29d4d9465b18d826818c669a88d728f14a7
KolTSinDTU <s253026@dtu.dk>
2026-06-09 10:19:38 +0200

    Create e1.py

---
+++ Week1/Day2/e1.py
@@ -0,0 +1,5 @@
+"""In this exercise, you may use built-in procedures for generating random numbers.
+Compare the results obtained from simulation with the corresponding theoretical results.
+Use histograms and statistical tests where appropriate.
+"""
+
